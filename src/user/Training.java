875d1a22aebe53ce47c01d41250a6f0063e8acc7
WU, Zhizhang <2874486w@student.gla.ac.uk>
2024-03-13 21:50:10 +0000

    优化ui

--- src/user/Training.java
+++ src/user/Training.java
@@ -1,76 +1,75 @@
 package user;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
 
 import Entity.Teacher;
 import Entity.TeachingRequirement;
-import Logic.BasicCommands;
-import Logic.Database;
+
+import Logic.*;
 
 public class Training {
+//    private static String requestfile ="src\\conf\\Teaching_Requirement.txt"; //The fine name of requirement
+//    private static String teacherfile ="src\\conf\\Teacher.txt";  //The file name of the teacher
 
     private static Database database;
 
-    // Constructor for Training class
+    // 构造函数接受 DataBase 实例
     public Training() {}
-
     private String name;
     private int id;
     private String skill;
+    //private static List<TeachingRequirement> teachingRequirements = BasicCommands.readTeachingRequirementsFromTxtFile(requestfile);
+    //private static List<Teacher> teacher = BasicCommands.readTeacherFromTxtFile(teacherfile);
+    //output all of available teacher
 
-    // Output teachers who are not assigned
+  //  public Training(String requestfile, String teacherfile){
+   //     teachingRequirements = BasicCommands.readTeachingRequirementsFromTxtFile(requestfile);
+   //     teacher = BasicCommands.readTeacherFromTxtFile(teacherfile);
+   // }
     private void outputTeacher(List<Teacher> list){
         for (Teacher te : list) {
-            if(!te.checkAssign()) System.out.println(te);
+            if(!te.checkAssign())System.out.println(te);
         }
     }
 
-    // Output all pending teaching requirements
+    //output all of the pending requirement
     private void outputrequirement(List<TeachingRequirement> list){
         for (TeachingRequirement tr : list) {
-            if(tr.checkPending()) System.out.println(tr);
+            if(tr.checkPending())System.out.println(tr);
         }
     }
 
-    // Train a teacher (teacher ID) for the skill of a request (requestID)
+    //Training a teacher(teacher ID) for the skill of request(requestID)
     private void Train(List<TeachingRequirement> request, List<Teacher> teacher, int requestID, int teacherID){
         TeachingRequirement requirement = null;
         boolean isChanged = false;
-        boolean notFound = true;
-
-        // Iterate through teaching requirements to find the specified requestID
-        for(TeachingRequirement tr: request){
+        boolean Notfound = true;
+        for(TeachingRequirement tr:request){
             if(tr.getRequestId() == requestID){
                 if(!tr.checkPending()){
                     System.out.println("Not Pending");
                     return;
-                } else {
+                }else {
                     requirement = tr;
-                    notFound = false;
+                    Notfound = false;
                 }
             }
         }
-
-        // Check if the requirement was found
-        if (notFound) {
+        if (Notfound) {
             System.out.println("Request Not Found");
             return;
         }
-
-        // Iterate through teachers to find the specified teacherID
-        for(Teacher te: teacher){
+        for(Teacher te:teacher){
             if(te.getId() == teacherID){
                 if(te.checkAssign()){
                     System.out.println("Already Assigned");
                     return;
-                } else {
+                }else {
                     if(te.checkSkill(requirement.getRequirement())){
                         System.out.println("Already Learned");
                         return;
-                    } else {
+                    }else{
                         te.setTrain(requirement.getRequirement());
                         te.setAssign();
                         requirement.setRequestStatus("approved");
@@ -79,75 +78,75 @@ public class Training {
                 }
             }
         }
-
-        // Check if any changes were made
         if(!isChanged){
             System.out.println("Teacher Not Found");
             return;
         }
         System.out.println("Trained");
+
     }
 
-    // Reject a requirement with a specific ID
+    //Order of rejecting a requirement with ID
     private void Rejecting(List<TeachingRequirement> list, int ID){
-        boolean isPending = false;
-        boolean isChange = false;
-
-        // Iterate through teaching requirements to find the specified ID
-        for(TeachingRequirement te: list){
-            if(te.getRequestId() == ID){
-                if(te.checkPending()){
-                    te.setRequestStatus("rejected");
-                    isChange = true;
-                } else isPending = true;
+            boolean isPending = false;
+            boolean isChange = false;
+            // 读取文件内容
+            for(TeachingRequirement te:list){
+                if(te.getRequestId() == ID){
+                    if(te.checkPending()){
+                        te.setRequestStatus("rejected");
+                        isChange = true;
+                    }else isPending = true;
+                }
             }
-        }
 
-        // Print appropriate messages based on the rejection status
-        if(isPending) System.out.println("Not Pending");
-        else if(!isChange) System.out.println("Request Not Found");
-        else System.out.println("Rejected");
+            if(isPending) System.out.println("Not Pending");
+            else if(!isChange) System.out.println("Request Not found");
+            else System.out.println("Rejected");
+
     }
 
-    // Main method for Training functionality
     public void training() {
-        // Read from file
+        //Read from file
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         database = new Database();
-
-        // Main loop
+        //Main loop
         while (true) {
             try {
-                // Output current state
+                //Output current state
                 System.out.println("Pending request:");
                 outputrequirement(database.getDataReader().getTeachingRequirements());
                 BasicCommands.writeline();
                 System.out.println("Available Teacher");
                 outputTeacher(database.getDataReader().getTeachers());
                 BasicCommands.writeline();
-                System.out.print("Enter request id and teacher id to match or Enter 'exit' to exit：");
-
-                // Read User Input from terminal
+                System.out.println("Enter train,request id and teacher id to match");
+                System.out.println("Enter reject,request id to reject");
+                System.out.println("Enter 'exit' to exit");
+                System.out.print("Command:");
+                //Read User Input from terminal
                 String userInput = reader.readLine();
-
-                // Exit command
+                //Exit command
                 if (userInput.equals("exit")) {
-                    // Write current data to the file
+                    //write current data to the file
                     database.getDataWriter().writeTeachingRequirements(database.getDataReader().getTeachingRequirements(), database.getDataReader().getRequestFilePath());
                     database.getDataWriter().writeTeachers(database.getDataReader().getTeachers(), database.getDataReader().getTeacherFilePath());
+//                    BasicCommands.writeTeachingRequirementsToTxtFile(database.getDataReader().getTeachingRequirements(), requestfile);
+//                    BasicCommands.writeTeacherToTxtFile(database.getDataReader().getTeachers(),teacherfile);
+                    System.out.println("Exiting");
                     break;
                 }
-
-                // Input Order checking
+                //Input Order checking
                 String[] Order = userInput.split(",");
-                if(Order.length != 2) System.out.println("Wrong Order");
-                else if(Order[0].equals("reject")) Rejecting(database.getDataReader().getTeachingRequirements(),Integer.parseInt(Order[1]));
-                else Train(database.getDataReader().getTeachingRequirements(), database.getDataReader().getTeachers(), Integer.parseInt(Order[0]),Integer.parseInt(Order[1]));
+                if(Order.length<2)System.out.println("Wrong Order");
+                else if(Order[0].equals("reject")&&Order.length == 2&&Order[1].matches( "\\d+"))Rejecting(database.getDataReader().getTeachingRequirements(),Integer.parseInt(Order[1]));
+                else if(Order[0].equals("train")&&Order[1].matches( "\\d+")&&Order[2].matches( "\\d+")&&Order.length == 3)Train(database.getDataReader().getTeachingRequirements(), database.getDataReader().getTeachers(), Integer.parseInt(Order[1]),Integer.parseInt(Order[2]));
+                else System.out.println("Wrong Order");
                 BasicCommands.writeline();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-}
 
+}
